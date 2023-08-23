f1226579e993bb8ce8722bfe146a9c74cfde877a
warren <diarbao@gmail.com>
2023-08-23 09:49:15 +0800

    添加mybatis flex 和生成代码

---
+++ backend-user-service/src/test/java/CodeGen.java
@@ -0,0 +1,87 @@
+import com.mybatisflex.codegen.Generator;
+import com.mybatisflex.codegen.config.ColumnConfig;
+import com.mybatisflex.codegen.config.GlobalConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class CodeGen {
+
+    public static void main(String[] args) {
+        //配置数据源
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mysql://43.134.186.33:3306/ram?characterEncoding=utf-8");
+        dataSource.setUsername("ram");
+        dataSource.setPassword("TYwKRD7wMjxT3GfK");
+
+        //创建配置内容，两种风格都可以。
+        GlobalConfig globalConfig = createGlobalConfigUseStyle1();
+        //GlobalConfig globalConfig = createGlobalConfigUseStyle2();
+
+        //通过 datasource 和 globalConfig 创建代码生成器
+        Generator generator = new Generator(dataSource, globalConfig);
+
+        //生成代码
+        generator.generate();
+    }
+
+    public static GlobalConfig createGlobalConfigUseStyle1() {
+        //创建配置内容
+        GlobalConfig globalConfig = new GlobalConfig();
+        //设置生成地址
+        globalConfig.getPackageConfig().setSourceDir("/Users/warren/code/backend-user/backend-user-service/src/main/java/");
+
+        //设置根包
+        globalConfig.setBasePackage("io.ram.payment");
+
+        //设置表前缀和只生成哪些表
+//        globalConfig.setTablePrefix("sys_");
+        globalConfig.setGenerateTable("payment_merchant_config", "payment_merchant_transfer_log");
+
+        //设置生成 entity 并启用 Lombok
+        globalConfig.setEntityGenerateEnable(true);
+        globalConfig.setEntityWithLombok(true);
+
+        //设置生成 mapper
+        globalConfig.setMapperGenerateEnable(true);
+        globalConfig.setServiceGenerateEnable(true);
+        globalConfig.setServiceImplGenerateEnable(true);
+        //可以单独配置某个列
+//        ColumnConfig columnConfig = new ColumnConfig();
+//        columnConfig.setColumnName("tenant_id");
+//        columnConfig.setLarge(true);
+//        columnConfig.setVersion(true);
+//        globalConfig.setColumnConfig("tb_account", columnConfig);
+
+        return globalConfig;
+    }
+
+    public static GlobalConfig createGlobalConfigUseStyle2() {
+        //创建配置内容
+        GlobalConfig globalConfig = new GlobalConfig();
+
+        //设置根包
+        globalConfig.getPackageConfig()
+                .setBasePackage("com.test");
+
+        //设置表前缀和只生成哪些表，setGenerateTable 未配置时，生成所有表
+        globalConfig.getStrategyConfig()
+                .setTablePrefix("tb_")
+                .setGenerateTable("tb_account", "tb_account_session");
+
+        //设置生成 entity 并启用 Lombok
+        globalConfig.enableEntity()
+                .setWithLombok(true);
+
+        //设置生成 mapper
+        globalConfig.enableMapper();
+
+        //可以单独配置某个列
+        ColumnConfig columnConfig = new ColumnConfig();
+        columnConfig.setColumnName("tenant_id");
+        columnConfig.setLarge(true);
+        columnConfig.setVersion(true);
+        globalConfig.getStrategyConfig()
+                .setColumnConfig("tb_account", columnConfig);
+
+        return globalConfig;
+    }
+}
